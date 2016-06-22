6f748a4d1750ce897d54c01248f194f0b8daff39
bures <richardbures@email.cz>
2016-06-22 15:50:34 +0200

    gridConfServ

---
+++ Service/GridConfigurationService.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rockuo
+ * Date: 21.6.16
+ * Time: 9:04
+ */
+
+namespace Trinity\Bundle\GridBundle\Service;
+
+use Trinity\Bundle\GridBundle\Grid\GridConfigurationBuilder;
+use Trinity\Bundle\SettingsBundle\Manager\SettingsManager;
+
+/**
+ * Class GridConfigurationService
+ * @package Necktie\AppBundle\Service
+ */
+class GridConfigurationService
+{
+    /** @var  SettingsManager $settingManager */
+    private $settingManager;
+
+    /**
+     * GridConfigurationService constructor.
+     * @param SettingsManager $settingsManager
+     */
+    public function __construct(SettingsManager $settingsManager)
+    {
+        $this->settingManager = $settingsManager;
+    }
+
+
+    /**
+     * @param string $url
+     * @param int $maxEntities
+     * @param int|null $limit
+     * @param bool $editable
+     * @return GridConfigurationBuilder
+     * @throws \Trinity\Bundle\SettingsBundle\Exception\PropertyNotExistsException
+     */
+    public function createGridConfigurationBuilder(
+        string $url,
+        int $maxEntities = 1,
+        int $limit = null,
+        bool $editable = false
+    ) {
+        if (!$limit) {
+            $limit = $this->settingManager->get('items_on_page');
+        }
+        return new GridConfigurationBuilder($url, $maxEntities, $limit, $editable);
+    }
+}
