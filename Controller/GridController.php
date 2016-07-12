4e322fd8e83536fbd474ec88b3040cad396604ba
Gabriel <Bordovsky.Gabriel@gmail.com>
2016-07-12 14:14:43 +0200

    Elastic search, score and paging support

--- Controller/GridController.php
+++ Controller/GridController.php
@@ -25,16 +25,20 @@ use Trinity\Bundle\SearchBundle\Search;
  */
 class GridController extends FOSRestController
 {
-
     /**
      * @Route("/elastic/{entity}", name="grid-elastic")
      *
+     * @QueryParam(name="c", nullable=true, strict=true, description="Columns", allowBlank=false)
      * @QueryParam(name="q", nullable=false, strict=true, description="DB Query", allowBlank=true)
+     * @QueryParam(name="offset", nullable=true, strict=true, description="Offset", allowBlank=false)
+     * @QueryParam(name="limit", nullable=true, strict=true, description="Limit", allowBlank=false)
+     * @QueryParam(name="orderBy", nullable=true, strict=true, description="Order by", allowBlank=false)
      *
      * @param string $entity
      * @param ParamFetcher $paramFetcher
      *
      * @return JsonResponse
+     * @throws \Doctrine\ORM\ORMException
      *
      * @throws \Trinity\Component\Utils\Exception\MemberAccessException
      * @throws \Trinity\Bundle\SearchBundle\Exception\SyntaxErrorException
@@ -44,7 +48,11 @@ class GridController extends FOSRestController
     public function gridElasticAction(ParamFetcher $paramFetcher, $entity)
     {
         $query = $paramFetcher->get('q');
-
+        $queryColumns = $paramFetcher->get('c');
+        $offset = $paramFetcher->get('offset');
+        $limit = $paramFetcher->get('limit');
+        $orderBy = $paramFetcher->get('orderBy');
+        
         /** @var GridManager $gridManager */
         $gridManager = $this->get('trinity.grid.manager');
 
@@ -52,25 +60,26 @@ class GridController extends FOSRestController
         $search = $this->get('trinity.search');
 
         /** @var NQLQuery $nqlQuery */
-        $nqlQuery = $search->queryTable($entity, $query);
-
+        if ($queryColumns === null) {
+            /** @var NQLQuery $nqlQuery */
+            $nqlQuery = $search->queryTable($entity, $query);
+        } else {
+            /** @var NQLQuery $nqlQuery */
+            $nqlQuery = $search->queryEntity($entity, $queryColumns, null, '', $limit, $offset, $orderBy);
+        }
         $columns = [];
 
         foreach ($nqlQuery->getSelect()->getColumns() as $column) {
             $columns[] = $column->getFullName();
         }
 
-        $entities = $this->get('trinity.logger.elastic_read_log_service')->getByQuery($nqlQuery);
-
-//        return new JsonResponse(
-//            $gridManager->convertEntitiesToArray($entities, $columns)
-//        );
+        list($entities, $totalCount, $scores) = $this->get('trinity.logger.elastic_read_log_service')
+            ->getByQuery($nqlQuery, $queryColumns?$query:'');
 
         $result = $gridManager->convertEntitiesToArray($entities, $columns);
 
-        //Gabi-TODO: when search/query/WHERE part is done solve this
         return new JsonResponse(
-            ['count' => ['result' => count($result), 'total' => count($result)], 'result' => $result]
+            ['count' => ['result' => count($result), 'total' => $totalCount], 'result' => $result, $scores]
         );
 
     }
@@ -134,7 +143,8 @@ class GridController extends FOSRestController
             $columns
         );
 
-        $totalCount = $this->get('trinity.search.dql_converter')->convertToCount($queryBuilder)->getQuery()->getSingleScalarResult();
+        $totalCount = $this->get('trinity.search.dql_converter')
+            ->convertToCount($queryBuilder)->getQuery()->getSingleScalarResult();
 
         return new JsonResponse(
             ['count' => ['result' => count($result), 'total' => $totalCount], 'result' => $result]
